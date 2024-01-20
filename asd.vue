8bf0deb0767fe3d5c1bcf7929e05571aaceae962
jean <jean@aprsupport.net>
2024-01-20 18:53:27 -0500

    adsada

--- asd.vue
+++ asd.vue
@@ -2,28 +2,4 @@
   <!-- Código HTML del componente -->
   <div>
     <h1>{{ mensaje }}</h1>
-    <button @click="cambiarMensaje">Cambiar Mensaje</button>
-  </div>
-</template>
 
-<script>
-export default {
-  data() {
-    return {
-      mensaje: "¡Hola, mundo!"
-    };
-  },
-  methods: {
-    cambiarMensaje() {
-      this.mensaje = "Nuevo mensaje";
-    }
-  }
-};
-</script>
-
-<style scoped>
-/* Estilos CSS específicos del componente */
-h1 {
-  color: green;
-}
-</style>
